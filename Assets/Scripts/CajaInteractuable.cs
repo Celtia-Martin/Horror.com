99f2a81dd80f93ca52f63b70d7e0522bc9796bf5
Celtia <c.martinga.2017@alumnos.urjc.es>
2021-04-01 22:36:33 +0200

    Subir Proyecto

---
+++ Assets/Scripts/CajaInteractuable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CajaInteractuable : MonoBehaviour
+{
+    private InputAction myAction;
+    [Space] [SerializeField] private InputActionAsset myActionsAsset;
+    private Vector3 initialPosition;//Secondary Button Right
+    private void Start()
+    {
+        initialPosition = transform.position;
+        myAction = myActionsAsset.FindAction("XRI LeftHand/RecuperarCaja");
+    }
+    private void Update()
+    {
+        if (myAction.triggered)
+        {
+            transform.position = initialPosition;
+        }
+    }
+}
---
+++ Assets/Scripts/InteraccionesCaja.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InteraccionesCaja : MonoBehaviour
+{
+ 
+    public void SetRed()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    
+    }
+    public void SetBlue()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+
+    }
+}
