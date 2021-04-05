22c24fea7fd2a4e9f010597b4b38ec5f16514fe9
Celtia <c.martinga.2017@alumnos.urjc.es>
2021-04-05 13:04:05 +0200

    Pisico

---
+++ Assets/Scripts/ScriptsHorror/MonsterIA.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterIA : MonoBehaviour
+{
+   [SerializeField] private Transform[] targets;
+    private float[,] stages = {{ 10, 30 },{6,28 },{ 5, 20 },{ 3, 15 },{ 3, 10 },{ 2, 5 } };
+
+
+
+    private void Start()
+    {
+        
+    }
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(1);
+    }
+
+}
---
+++ Assets/Scripts/ScriptsHorror/MovilController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovilController : MonoBehaviour
+{
+    [SerializeField]private Camera cFrontal;
+    [SerializeField] private Camera cInterior;
+    [SerializeField] private Image display;
+    private bool movilActivado=true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movilActivado)
+        {
+            Renderizado(cFrontal);
+        }
+        
+    }
+
+    void Renderizado(Camera target)
+    {
+        var current = RenderTexture.active;
+        RenderTexture.active = target.targetTexture;
+        target.Render();
+        Texture2D pantalla = new Texture2D(target.targetTexture.width, target.targetTexture.height);
+        pantalla.ReadPixels(new Rect(0, 0, target.targetTexture.width, target.targetTexture.height), 0, 0);
+        pantalla.Apply();
+        RenderTexture.active = current;
+        display.sprite = Sprite.Create(pantalla, new Rect(0, 0, target.targetTexture.width, target.targetTexture.height), new Vector2(0, 0));
+        
+        
+    }
+}
